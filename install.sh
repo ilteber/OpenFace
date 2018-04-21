fdd159cce9f006564524747624e370f4a23a9764
ilteber <ilteber@users.noreply.github.com>
2018-04-21 21:44:51 +0300

    opencv installation removed

--- install.sh
+++ install.sh
@@ -36,20 +36,20 @@ sudo apt-get -y install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev l
 echo "Essential dependencies installed."
 
 # OpenCV Dependency
-echo "Downloading OpenCV..."
-wget https://github.com/opencv/opencv/archive/3.4.0.zip
-unzip 3.4.0.zip
-cd opencv-3.4.0
-mkdir -p build
-cd build
-echo "Installing OpenCV..."
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_CUDA=OFF -D BUILD_SHARED_LIBS=OFF ..
-make -j4
-sudo make install
-cd ../..
-rm 3.4.0.zip
-sudo rm -r opencv-3.4.0
-echo "OpenCV installed."
+#echo "Downloading OpenCV..."
+#wget https://github.com/opencv/opencv/archive/3.4.0.zip
+# unzip 3.4.0.zip
+# cd opencv-3.4.0
+# mkdir -p build
+# cd build
+# echo "Installing OpenCV..."
+# cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_CUDA=OFF -D BUILD_SHARED_LIBS=OFF ..
+# make -j4
+# sudo make install
+# cd ../..
+# rm 3.4.0.zip
+# sudo rm -r opencv-3.4.0
+# echo "OpenCV installed."
 
 # Boost C++ Dependency
 echo "Installing Boost..."
