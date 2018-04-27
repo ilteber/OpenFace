02d263e5a4e2d886791d1471b82f0baffe25458d
ilteber <ilteber@users.noreply.github.com>
2018-04-27 17:50:12 +0300

    type

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ sudo apt-get -y install git libgtk2.0-dev pkg-config libavcodec-dev libavformat-
 sudo apt-get -y install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev checkinstall
 echo "Essential dependencies installed."
 
-OpenCV Dependency
+# OpenCV Dependency
 echo "Downloading OpenCV..."
 wget https://github.com/opencv/opencv/archive/3.4.0.zip
 unzip 3.4.0.zip
