7af8c0bac27727ead338ecf79e78517f03d162a5
ilteber <ilteber@users.noreply.github.com>
2018-04-27 17:49:08 +0300

    opencv installation editted

--- install.sh
+++ install.sh
@@ -35,21 +35,21 @@ sudo apt-get -y install git libgtk2.0-dev pkg-config libavcodec-dev libavformat-
 sudo apt-get -y install python-dev python-numpy libtbb2 libtbb-dev libjpeg-dev libpng-dev libtiff-dev libjasper-dev libdc1394-22-dev checkinstall
 echo "Essential dependencies installed."
 
-# OpenCV Dependency
-#echo "Downloading OpenCV..."
-#wget https://github.com/opencv/opencv/archive/3.4.0.zip
-# unzip 3.4.0.zip
-# cd opencv-3.4.0
-# mkdir -p build
-# cd build
-# echo "Installing OpenCV..."
-# cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_CUDA=OFF -D BUILD_SHARED_LIBS=OFF ..
-# make -j4
-# sudo make install
-# cd ../..
-# rm 3.4.0.zip
-# sudo rm -r opencv-3.4.0
-# echo "OpenCV installed."
+OpenCV Dependency
+echo "Downloading OpenCV..."
+wget https://github.com/opencv/opencv/archive/3.4.0.zip
+unzip 3.4.0.zip
+cd opencv-3.4.0
+mkdir -p build
+cd build
+echo "Installing OpenCV..."
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_CUDA=OFF -D BUILD_SHARED_LIBS=OFF ..
+make -j4
+sudo make install
+cd ../..
+rm 3.4.0.zip
+sudo rm -r opencv-3.4.0
+echo "OpenCV installed."
 
 # Boost C++ Dependency
 echo "Installing Boost..."
